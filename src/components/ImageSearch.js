5c00ccef7c50e73f09333f343375e9d318236bd2
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-08-29 00:25:12 +0530

    Added Search bar

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ImageSearch = () => {
+const ImageSearch = ({ searchText }) => {
+    const [text, setText ] = useState('');
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        searchText(text);
+    }
     return (
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
-            <form className="w-full max-w-sm">
-                <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-                    <input type="text" className="appearance-none bg-transparent border-none w-full 
+            <form onSubmit={onSubmit} className="w-full max-w-sm">
+                <div className="flex items-center border-b border-b-2 border-green-500 py-2">
+                    <input value={text} onChange={((e) => setText(e.target.value))} type="text" className="appearance-none bg-transparent border-none w-full 
                     text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
                     placeholder="Search Image Term..."
                     />
-                    <button type="submit" className="flex-shrink-0 bg-green-500 hover:bg-green-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+                    <button type="submit" className="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded">
                         Search
                     </button>
                 </div>
