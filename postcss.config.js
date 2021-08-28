02074f8434449fb3eb7bef0f66b1e73c5660bfca
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-08-29 00:00:17 +0530

    Added components

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-const tailwindcss =  require('tailwind');
-
+const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
-        require('autoprefix')
-    ]
-}
+        require('autoprefixer')
+    ],
+};
